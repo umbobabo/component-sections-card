07753872022663c21422caf0868ae159cf9289f0
Gary Siladi <gergely.siladi@globallogic.com>
2016-10-13 12:28:13 +0200

    fix: WP-89 - allow optional other type of SectionCardList

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import SectionCardList from './parts/section-card-list';
 
 export default function SectionsCard({ data, titles }) { // eslint-disable-line id-blacklist
   const links = data; // eslint-disable-line id-blacklist
+  const sectionsSection = <SectionCardList topic="sections" links={links.sections} title={titles.sections} />;
   return (
     <nav role="nav" className="sections-card">
       <div className="sections-card__wrapper">
         <div className="sections-card__menu">
-          <SectionCardList topic="sections" links={links.sections} title={titles.sections} />
+          {typeof links.other === 'undefined' ? sectionsSection :
+              <div className="sections-card__list-column-wrap">
+                {sectionsSection}
+                <SectionCardList topic="other" links={links.other} title={titles.other} />
+              </div>
+          }
           <SectionCardList topic="media" links={links.media} title={titles.media} />
           <SectionCardList topic="blogs" links={links.blogs} title={titles.blogs} />
         </div>
